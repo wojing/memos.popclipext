4ab029e15336ab73730b3c3d7e6c8ed143f63ea2
woKing <wojing000@gmail.com>
2023-10-10 00:42:07 +0800

    Fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # memos.popclipext
-a popclip extension for memos 
+A PopClip extension for Memos.
+
+## USAGE
+1. Download the zip file and rename it to end with '.popclipext'.
+2. Double-click the package to install it.
+3. Set your website and access token.
+4. That's it, you're ready to go.
+
+## License
+This project is licensed under the terms of the MIT license. See [LICENSE](https://opensource.org/license/mit/) for more detail.
+
--- memos.js
+++ memos.js
@@ -1,16 +1,10 @@
 // memos.js
 
-
 const query = popclip.input.text
 
 const access_token = popclip.options.access_token
 
 const url = popclip.options.site
-
-// // curl -X POST  https://memos.wojin.eu.org/api/v1/memo \
-// //    -H "Accept: application/json" \
-// //    -H "Authorization: Bearer {YOUR_ACCESS_TOKEN}"
-// //    -d '{'content': 'Hello World!'}'
 
 const axios = require('axios');
 
@@ -31,5 +25,3 @@ axios.post(url, data, {headers})
     console.log(err);
   }); 
 
-
-//https://www.popclip.app/dev/config#the-options-array
